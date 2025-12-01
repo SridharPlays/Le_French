03ace82896e0f3e28e58ffbd67ba8786a53557f0
Sridhar <entertainmentsridhar@gmail.com>
2025-12-01 16:16:43 +0530

    Production Fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,6 +26,8 @@ app.get('/', (req, res) => {
   res.send('French Gamification API is running');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+export default app;
